a00fa73f80187e8ce8a3caf981c102ef830a5048
josepereza <48152618+josepereza@users.noreply.github.com>
2019-04-18 16:22:20 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,14 +2,14 @@
 
 /*
 // mysql_connect("database-host", "username", "password")
-$conn = mysql_connect("localhost","root","12345Seis") 
+$conn = mysql_connect("localhost","root","123456Seis") 
 			or die("cannot connected");
 
 // mysql_select_db("database-name", "connection-link-identifier")
 @mysql_select_db("test",$conn);
 */
 
-$mysqli = mysqli_connect("localhost", "root", "12345Seis", "test");
+$mysqli = mysqli_connect("localhost", "root", "123456Seis", "test");
 
 //$res = mysqli_query($mysqli, "SELECT 'Please, do not use ' AS _msg FROM DUAL");
 //$row = mysqli_fetch_assoc($res);
